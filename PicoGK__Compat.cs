fac05be2b7eef26f82679306de1307e1ba401a29
Alex Schiftner <alex@shapediver.com>
2024-01-07 18:21:53 +0100

    add further compat methods used by ShapeKernel

--- PicoGK__Compat.cs
+++ PicoGK__Compat.cs
@@ -45,13 +45,18 @@ namespace PicoGK
 #if !NETSTANDARD2_1_OR_GREATER && !NETCOREAPP2_0_OR_GREATER
     public static class MathF
     {
+        public static float Abs(float x) { return (float)Math.Abs(x); }
+        public static float Acos(float x) { return (float)Math.Acos(x); }
+        public static float Atan2(float x, float y) { return (float)Math.Atan2(x, y); }
         public static float Ceiling(float x) { return (float)Math.Ceiling(x); }
         public static float Cos(float x) { return (float)Math.Cos(x); }
         public static float Max(float x, float y) { return (float)Math.Max(x, y); }
+        public static float Min(float x, float y) { return (float)Math.Min(x, y); }
         public static float PI => (float)Math.PI;
         public static float Pow(float x, float y) { return (float)Math.Pow(x, y); }
         public static float Sin(float x) { return (float)Math.Sin(x); }
         public static float Sqrt(float x) { return (float)Math.Sqrt(x); }
+        public static float Tan(float x) { return (float)Math.Tan(x); }
     }
 
 #endif
